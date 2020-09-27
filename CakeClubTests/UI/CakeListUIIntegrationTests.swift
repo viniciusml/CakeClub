bf0665885505b80099850d183c49ec237ee555c8
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2020-09-27 18:38:52 +0100

    'CakeListViewController' does not make request on initialisation

--- CakeClubTests/UI/CakeListUIIntegrationTests.swift
+++ CakeClubTests/UI/CakeListUIIntegrationTests.swift
@@ -6,9 +6,25 @@
 //  Copyright © 2020 Vinicius Leal. All rights reserved.
 //
 
+import CakeClub
 import XCTest
 
+class CakeViewModel {
+    private let cakeLoader: CakeLoader
+
+    init(cakeLoader: CakeLoader) {
+        self.cakeLoader = cakeLoader
+    }
+}
+
 class CakeListViewController: UIViewController {
+
+    private var viewModel: CakeViewModel?
+
+    convenience init(viewModel: CakeViewModel) {
+        self.init()
+        self.viewModel = viewModel
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,5 +41,24 @@ class CakeListUIIntegrationTests: XCTestCase {
         sut.loadViewIfNeeded()
 
         XCTAssertEqual(sut.title, "Would you have some cake?")
+    }
+
+    func test_init_doesNotMakeRequest() {
+        let loader = RemoteCakeLoaderSpy()
+        let viewModel = CakeViewModel(cakeLoader: loader)
+        _ = CakeListViewController(viewModel: viewModel)
+
+        XCTAssertEqual(loader.loadCallCount, 0)
+    }
+
+    // MARK: - Helpers
+
+    class RemoteCakeLoaderSpy: CakeLoader {
+
+        private(set) var loadCallCount: Int = 0
+
+        func load(completion: @escaping (CakeLoader.Result) -> Void) {
+            loadCallCount += 1
+        }
     }
 }
