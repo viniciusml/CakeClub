26971fc40fa3750df77c3e7108169f4c07d9d84f
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2020-09-27 16:06:20 +0100

    Inject 'URL' instead of hardcoding it in 'RemoteCakeLoader'

--- CakeClubTests/RemoteCakeLoaderTests.swift
+++ CakeClubTests/RemoteCakeLoaderTests.swift
@@ -9,14 +9,16 @@
 import XCTest
 
 class RemoteCakeLoader {
+    let url: URL
     let client: HTTPClient
 
-    init(client: HTTPClient) {
+    init(url: URL, client: HTTPClient) {
+        self.url = url
         self.client = client
     }
 
     func load() {
-        client.get(from: URL(string: "https://a-url.com")!)
+        client.get(from: url)
     }
 }
 
@@ -26,15 +28,17 @@ protocol HTTPClient {
 
 class RemoteCakeLoaderTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
+        let url = URL(string: "https://a-url.com")!
         let client = HTTPClientSpy()
-        _ = RemoteCakeLoader(client: client)
+        _ = RemoteCakeLoader(url: url, client: client)
 
         XCTAssertNil(client.requestedURL)
     }
 
     func test_load_requestsDataFromURL() {
+        let url = URL(string: "https://a-url.com")!
         let client = HTTPClientSpy()
-        let sut = RemoteCakeLoader(client: client)
+        let sut = RemoteCakeLoader(url: url, client: client)
 
         sut.load()
 
