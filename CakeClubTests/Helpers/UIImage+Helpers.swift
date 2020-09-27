9a690db5d4a3c38ce702ad45898984548c9c5eac
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2020-09-27 17:25:36 +0100

    Move 'UIImage' test helpers to its own file

---
+++ CakeClubTests/Helpers/UIImage+Helpers.swift
@@ -0,0 +1,26 @@
+//
+//  UIImage+Helpers.swift
+//  CakeClubTests
+//
+//  Created by Vinicius Leal on 27/09/2020.
+//  Copyright © 2020 Vinicius Leal. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    static func make(withColor color: UIColor) -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()!
+        context.setFillColor(color.cgColor)
+        context.fill(rect)
+        let img = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return img!
+    }
+
+    static func makePlaceholder() -> UIImage {
+        UIImage(named: "cake-placeholder")!
+    }
+}
