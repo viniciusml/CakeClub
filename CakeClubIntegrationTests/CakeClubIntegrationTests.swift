6c2968d734ef7900a4fbe7f279d18fce915953f3
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2020-09-27 17:29:01 +0100

    Add 'CakeClubIntegrationTests' target, to run integration tests separately

---
+++ CakeClubIntegrationTests/CakeClubIntegrationTests.swift
@@ -0,0 +1,14 @@
+//
+//  CakeClubIntegrationTests.swift
+//  CakeClubIntegrationTests
+//
+//  Created by Vinicius Leal on 27/09/2020.
+//  Copyright © 2020 Vinicius Leal. All rights reserved.
+//
+
+import XCTest
+
+class CakeClubIntegrationTests: XCTestCase {
+
+    func test() {}
+}
