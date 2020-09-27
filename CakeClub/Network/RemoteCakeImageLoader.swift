dadbcb5bf2eec4d36cfc3392ec46e8c791fa9495
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2020-09-27 17:23:59 +0100

    Create 'RemoteCakeImageLoader' in preparation to write integration tests

---
+++ CakeClub/Network/RemoteCakeImageLoader.swift
@@ -0,0 +1,15 @@
+//
+//  RemoteCakeImageLoader.swift
+//  CakeClub
+//
+//  Created by Vinicius Leal on 27/09/2020.
+//  Copyright © 2020 Vinicius Leal. All rights reserved.
+//
+
+import UIKit
+
+public class RemoteCakeImageLoader: CakeImageLoader {
+    public func loadImage(from url: URL, into view: UIImageView) {
+
+    }
+}
